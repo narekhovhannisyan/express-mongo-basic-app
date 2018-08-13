8f6ca7a3c67e33d554e2d4d02e6b9290b4741773
narekhovhannisyan <narekgilmour@gmail.com>
2018-08-13 21:24:21 +0400

    app: initialize 404 error middleware in app.js file
     - fixes #9

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const app = express()
 
+const { PathNotFoundError } = require('./util').Errors
+
 const healthApi = require('./routes/health/health.api')
 
 /**
@@ -17,5 +19,10 @@ app.use(bodyParser.json({ limit: '5mb' }))
  * @description Add health API (NO authorization, NO api prefix).
  */
 app.use('/health', healthApi)
+
+/**
+ * @description Middleware - catch 404 and forward to error handler.
+ */
+app.use((req, res, next) => next(new PathNotFoundError('The specified resource path does not exist.')))
 
 module.exports = app
